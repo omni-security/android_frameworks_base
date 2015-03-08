2d47ed22a2873f0249abc6ecf932d4ead52e123a
Justin M <justinjams@gmail.com>
2015-03-08 01:33:42 -0500

    Themes: Style "Android is Upgrading" dialog to Material Dark
    Change-Id: I60843689f29ca3304ed2e17218ca8541efd14b73

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -6593,6 +6593,8 @@ public class PhoneWindowManager implements WindowManagerPolicy {
                     } else if (mContext.getPackageManager().hasSystemFeature(
                             PackageManager.FEATURE_TELEVISION)) {
                         theme = com.android.internal.R.style.Theme_Leanback_Dialog_Alert;
+                    } else if (SystemProperties.get("ro.product.board").equals("tuna")) {
+                        theme = com.android.internal.R.style.Theme_Material_Dialog_Alert;
                     } else {
                         theme = com.android.internal.R.style.Theme_Material_DayNight_Dialog_Alert;
                     }
