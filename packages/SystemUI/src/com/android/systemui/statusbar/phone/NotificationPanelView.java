b3f94af5c5ada23a592bbbdc6f8935416b517730
Tony Mak <tonymak@google.com>
2017-07-28 11:53:56 +0100

    Enforce policy for camera gesture in keyguard
    Test:
    1. Set lock screen, set keyguard policy. Lock the device.
       Observe that double tap is not showing camera
    
    2. Set lock screen, unset the keyguard policy. Lock the device.
       Observe that double tap is showing camera
    
    3. Unset lock screen (swipe), set the keyguard policy. Lock the device.
       Observe that double tap is showing camera.
    
    4. Unset lock screen (swipe), unset the keyguard policy. Lock the device.
       Observe that double tap is showing camera.
    
    Bug: 63787722
    Merged-In: I104688eaad719528376e2851f837d5956a6a1169
    
    Change-Id: I42e6d9015682998176fe41971356bde22e1b37b2
    (cherry picked from commit 65f02e8ba7a9f013d6971b3d6d1bd95f1785cb3d)

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NotificationPanelView.java
@@ -36,6 +36,7 @@ import android.provider.Settings;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.util.MathUtils;
+import android.util.EventLog;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
@@ -2485,6 +2486,10 @@ public class NotificationPanelView extends PanelView implements
      * @param keyguardIsShowing whether keyguard is being shown
      */
     public boolean canCameraGestureBeLaunched(boolean keyguardIsShowing) {
+        if (!mStatusBar.isCameraAllowedByAdmin()) {
+            EventLog.writeEvent(0x534e4554, "63787722", -1, "");
+            return false;
+        }
         ResolveInfo resolveInfo = mKeyguardBottomArea.resolveCameraIntent();
         String packageToLaunch = (resolveInfo == null || resolveInfo.activityInfo == null)
                 ? null : resolveInfo.activityInfo.packageName;
